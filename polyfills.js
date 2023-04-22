9a06208dd8a37569a0e0c3e8fec7ebe7e2bbd734
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-22 17:16:13 +0530

    custom filter(Polyfills)

---
+++ polyfills.js
@@ -0,0 +1,38 @@
+// MAP Polyfills
+
+let arr=[1,2,3,4,5,6,7,8,9,10];
+// let squaredArr=arr.map(function(x){
+//     return x*x;
+// })
+
+// console.log(arr);
+// console.log(squaredArr);
+
+//custom map function
+// cb--> callback function
+// function myPolyfill(arr, cb){
+//     let newArr=[];
+
+//     for(let i=0;i<arr.length;i++){
+//         newArr.push(cb(arr[i]));
+//     }
+// }
+
+// function square(x){
+//     return x*x;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
