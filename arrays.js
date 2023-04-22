da9865ba2d8cea16f8dcf658e1838bea8cea329f
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-22 17:12:14 +0530

    Arrays in javaScript

---
+++ arrays.js
@@ -0,0 +1,47 @@
+// Arrays
+//JavaScript array is a single variable that is used to store different elements. 
+//It is often used when we want to store a list of elements and access them by a single variable.
+  
+// declaration
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+// console.log("-------------------");
+// console.log(arr.length);
+// let i=0;
+// while(i<arr.length){
+//     console.log("element in index",i,"is",arr[i]);
+//     i++;
+// }
+
+// for adding at last --> push
+// for adding at first --> unshift
+
+// arr.push("last value");
+// arr.unshift("first value");
+// console.log("----------------------------");
+// console.log(arr);
+
+// for removing element from last--> pop.
+// for removing element from first--> shift.
+
+// arr.pop();
+// arr.shift();
+
+// console.log("----------------------------");
+// console.log(arr);
+
+// let partOfAnArray=arr.slice(1,3);
+// console.log(partOfAnArray);
+// console.log("----------------------------");
+// console.log(arr);
+
+// arr.splice(2,3);
+// console.log("----------------------------");
+// console.log(arr);
+
+
+
+
+
+
+
