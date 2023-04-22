b51ae62ffeac39a28c14072e7d250ef145e5f983
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-22 17:10:32 +0530

    string concept in java Script

---
+++ string.js
@@ -0,0 +1,35 @@
+//Strings are texts that help in holding data that can be represented. 
+//A JavaScript string stores a series of characters like “CaptainAmerica”. 
+//A string can be any text inside double or single quotes.
+
+let singleQuoteString='CaptainAmerica';
+let doubleQuoteString="CaptainAmerica";
+console.log(singleQuoteString);
+console.log(doubleQuoteString);
+
+// console.log("---------------------------------")
+// let char=singleQuoteString.charAt(4);
+// console.log(char);
+// console.log("---------------------------------")
+// let subStr=singleQuoteString.substring(1,4);
+// console.log(subStr);
+
+
+// SPLIT();
+// let arrStr=singleQuoteString.split("i");
+// console.log(arrStr);
+// let arrStr1=singleQuoteString.split("");
+// console.log(arrStr1);
+
+//JOIN();
+
+// let arrStr=singleQuoteString.split("i");
+// console.log(arrStr);
+// let join=arrStr.join("$");
+// console.log(join);
+
+// trim()--> remove white spaces from last and starting.
+
+
+
+
