933c9205abeef6848d7d48573f68d32350f47bdf
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-22 17:09:30 +0530

    Understanding declarations

---
+++ let_const.js
@@ -0,0 +1,28 @@
+//let
+
+//let variable is a block scoped variable. It means that the variable is only accessible within the block in which it is declared. 
+//It is not accessible outside the block.
+//It is also not accessible before the declaration of the variable.
+
+// console.log("hello");
+//console.log(varName);
+
+// let varName;
+// console.log(varName);
+
+// varName=20;
+// varName=30;
+// console.log(varName);
+
+
+
+// const a=20;
+// console.log(a);
+
+
+// for loop
+
+let num=10;
+for(let i=0;i<10;i++){
+    console.log(i)
+}
