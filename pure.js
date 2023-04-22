7ebfd80bb339a8b70dae186c1cc1edfa22a102f6
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-22 17:14:57 +0530

    Pure and Impure functions

---
+++ pure.js
@@ -0,0 +1,9 @@
+//pure functions
+//A Pure Function is a function (a block of code) that always returns the same result if the same arguments are passed. 
+//It does not depend on any state or data change during a program’s execution.
+
+// function addNum(a,b){
+//     return a+b;
+// }
+// console.log(addNum(2,3)); 
+
