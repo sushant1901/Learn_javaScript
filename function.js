132868285168bb70811733d1f97e5d6c23369299
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-22 17:11:16 +0530

    Functions in javaScript

---
+++ function.js
@@ -0,0 +1,45 @@
+// console.log("line number 1",varName);
+// var varName = "Rohan";
+// console.log("line number 3",varName);
+// function fn(){
+//     console.log("line number 5",varName);
+//     var varName = "Harry";
+//     console.log("line number 3",varName);
+// }
+
+// fn();
+
+// Non-Primitive--> array,objects,function
+
+// function sayHi(param){// fn defination
+//     console.log("hello");
+//     console.log("param receive:",param);
+
+// }
+// sayHi();// fn call
+// sayHi(10);
+// sayHi("Sushant");
+// sayHi([1,2,3,4,5]);
+
+
+// function sayHi(param){
+
+//     return "returned from function."
+    
+// }
+
+// let rVal=sayHi([1,2,3,4,5]);
+// console.log("rVal is: ",rVal);
+
+
+// function sayHi(param){
+//     let rVal=Math.random()>0.5?true:"less than 0.5";
+//     return rVal;
+// }
+// let rVal=sayHi([1,2,3,4,5]);
+// console.log(rVal);
+
+
+
+
+
