2f0d6892305009691736872dd95bb0d297dabbda
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-04-22 17:09:46 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,52 @@
+// objects--> Group of key:value pairs.
+// when value is there it is called property.
+// when function is there it is called method.
+//Objects, in JavaScript, are the most important 
+//data type and form the building blocks for modern JavaScript.
+
+// let obj={
+//     name:"sushant",
+//     lName:"Mishra",
+//     address:{
+//         city:"Mathura",
+//         state:"Uttar Pradesh"
+//     },
+//     phone:"9696078980",
+//     skills:["java","c","python"],
+//     coder:true,
+//     sayHi:function(){
+//         console.log("Sushant say's Hi")
+//     }
+
+// };
+//GET
+// console.log(obj.name);
+// console.log(obj.lName);
+// console.log(obj.skills[1]);
+// obj.sayHi();
+// for(let key in obj){
+//     console.log(key,":",obj[key]);
+// }
+
+// SET or Update.
+
+// console.log("object:", obj);
+// obj.phone="7379502269";
+// obj.coder=false;
+// obj.friends=["shreyansh","hasrh","aryan"];
+
+// DELETE
+
+// delete obj.address;
+// console.log("-----------------------------------");
+// console.log("object:", obj);
+
+//Different ways to get value in objects.
+
+// console.log(obj.name);
+// let propKey="name";
+// console.log(obj[propKey]);
+// console.log(obj["name"]);
+
+ 
+
